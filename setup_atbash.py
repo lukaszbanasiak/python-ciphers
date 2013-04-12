02e85d083a50581419a46b220af3fa8c0f6e4829
Lukasz Banasiak <lukasz@banasiak.me>
2013-04-12 14:51:35 +0200

    add: zmiana znakow diakrytycznych

--- setup_atbash.py
+++ setup_atbash.py
@@ -4,7 +4,7 @@ import py2exe
 
 properties = {'script': "AtBash.py",
               'icon_resources': [(0, "encryption.ico")],
-              'version': "1.0",
+              'version': "1.1",
               'company_name': "labs @ PUT",
               'copyright': "Copyright 2013 Lukasz Banasiak. All rights reserved.",
               'name': "AtBash Cipher demo",
