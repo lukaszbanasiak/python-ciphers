7ce93e1e950d0ba6bb110d8b12eccb0b62209b3e
Łukasz Banasiak <lukasz@banasiak.me>
2014-11-30 13:12:56 +0100

    add entropy count, huffman coding, replace national chars to ascii equivalent

--- CharFreq.py
+++ CharFreq.py
@@ -1,92 +1,134 @@
 # -*- coding: utf-8 -*-
-import collections
-import optparse
+from collections import Counter
+from math import log, ceil
+from heapq import heappush, heappop, heapify
+from unicodedata import normalize as norm
 import codecs
-import sys
+import optparse
 import re
+import sys
 
-__author__ = 'Lukasz Banasiak <lukasz@banasiak.me>'
-__version__ = '1.0'
+__author__ = 'Lukasz Banasiak'
+__version__ = '1.1'
+__description__ = """In cryptanalysis, frequency analysis is the study
+ of the frequency of letters or groups of letters in a ciphertext."""
 
 
-def count(words, all_chars=False):
+def national2ascii(data):
+    return norm('NFKD', data).encode('ascii', 'ignore')
+
+
+def huffman(counter):
+    heap = [[wt, [sym, ""]] for sym, wt in counter.items()]
+    heapify(heap)
+    while len(heap) > 1:
+        lo = heappop(heap)
+        hi = heappop(heap)
+        for pair in lo[1:]:
+            pair[1] = '0' + pair[1]
+        for pair in hi[1:]:
+            pair[1] = '1' + pair[1]
+        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+    return sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+
+def fi(per, v):
+    return per/100 * int(ceil(v))
+
+
+def hi(per):
+    return -log(per/100, 2)
+
+
+def entropy(per):
+    return -(per/100 * log(per/100, 2))
+
+
+def percentage(p, lns):
+    return float(p) / float(lns) * 100
+
+
+def count(words, all_chars=False, normalize=False):
     """Character frequency analysis.
 
     A simple analysis of the characters in the text.
 
     :param words: string to count
     :param all_chars: set counting all characters
+    :param normalize: replace national characters to ASCII equivalents
     """
+
+    if normalize:
+        words = national2ascii(words)
+
     regex = re.compile(u'^[a-zA-ZĄĘŚĆŻŹŁÓĆŃąęśćżźłóćń]+$')
-    char_dict = collections.Counter()
+    counter = Counter()
 
     for char in words:
         if not all_chars:
             if regex.match(char):
-                char_dict[char] += 1
+                counter[char] += 1
         else:
             if not char in ['\r', '\n', u'\u2013']:
-                char_dict[char] += 1
+                counter[char] += 1
+    return counter
 
-    for char in sorted(char_dict, key=char_dict.get, reverse=True):
-        print '  %s: %d  (%.2f%%)' % (
-            char, char_dict[char], (float(char_dict[char])) / float(sum(char_dict.values())) * 100)
 
-    print '  +: %d' % (sum(char_dict.values()))  # TODO: more elegant functions return :)
+def _print(counter):
+    H = []
+    L = []
+    print 'Char\tCount\t\tPercent\tHi\tFi\tHuffman'
+    print '-'*70
+    for char in huffman(counter):
+        per = percentage(counter[char[0]], sum(counter.values()))
+        h = entropy(per)
+        H.append(h)
+        l = fi(per, hi(per))
+        L.append(l)
+        print '%s\t%d\t\t%.3f%%\t%.3f\t%.3f\t%s' % (char[0], counter[char[0]], per, hi(per), l, char[1])
+    print '-'*70
+    print '+:\t%s' % sum(counter.values())
+    print '='*70
+    print 'H: %s' % sum(H)
+    print 'L: %s' % sum(L)
 
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser(version=__version__,
-                                   usage='Usage: %prog [options] [args]',
-                                   description='In cryptanalysis, frequency analysis is the study of the frequency of letters or groups of letters in a ciphertext.')
-    parser.add_option('-a', dest='allchars', action='store_true', default=False,
-                      help='zliczaj wszystkie znaki')
-    parser.add_option('-c', dest='case_sensitive', action='store_true', default=False,
-                      help='rozroznianie semantyki malych i wielkich liter')
-    parser.add_option('-f', dest='file', action='store', default=False,
-                      help='wskaz plik z tekstem')
+    parser = optparse.OptionParser(version=__version__, usage='Usage: %prog [options] [args]',
+                                   description=__description__)
+    parser.add_option('-a', dest='allchars', action='store_true', default=True, help='count all chars')
+    parser.add_option('-c', dest='case_sensitive', action='store_true', default=True, help='case-sensitivity')
+    parser.add_option('-n', dest='normalize', action='store_true', default=False,
+                      help='replace national characters to ASCII equivalents')
+    parser.add_option('-f', dest='file', action='store', default=False, help='text file')
 
     (options, args) = parser.parse_args()
 
-    print ''
-    print 'Czestotliwosc wystepowania znakow'
-    print ''
-    print '       Autor: ' + __author__
-
-    if not options.allchars:
-        print '        Tryb: Zliczanie tylko liter'
-    else:
-        print '        Tryb: Zliczanie wszystkich znakow'
-
-    if options.case_sensitive:
-        print ' Case-sensi.: Tak'
-    else:
-        print ' Case-sensi.: Nie'
-
+    print 'Characters Frequency Analysis'
+    print 'Author: %s' % __author__
+    print 'Count all chars: %s' % options.allchars
+    print 'Case-sensitivity: %s' % options.case_sensitive
+    print 'Replace national characters to ASCII equivalents: %s' % options.normalize
+    print
     if options.file:
-        print ''
-        print ' IN> ' + options.file
-
         try:
             file_stream = codecs.open(options.file, 'r', 'dbcs')
-            if options.case_sensitive:
-                count(file_stream.read(), all_chars=options.allchars)
-            else:
-                count(file_stream.read().upper(), all_chars=options.allchars)
-            file_stream.close()
         except IOError as e:
-            print '\nI/O error({0}): {1}'.format(e.errno, e.strerror)
-            sys.exit(1)
-
+            print '%s: %s' % (e.strerror, options.file)
+        else:
+            print '>>> ' + options.file
+            text = file_stream.read()
+            if not options.case_sensitive:
+                text = text.upper()
+            _print(count(text, all_chars=options.allchars, normalize=options.normalize))
+            file_stream.close()
     else:
         while 1:
-            print ''
             try:
-                text = raw_input(' IN> ').decode(sys.stdin.encoding)
-            except (SystemExit, KeyboardInterrupt):
+                text = raw_input('>>> ').decode(sys.stdin.encoding)
+            except (SystemExit, KeyboardInterrupt) as e:
                 sys.exit(0)
-
-            if options.case_sensitive:
-                count(text, all_chars=options.allchars)
             else:
-                count(text.upper(), all_chars=options.allchars)
+                if not options.case_sensitive:
+                    text = text.upper()
+                _print(count(text, all_chars=options.allchars, normalize=options.normalize))
